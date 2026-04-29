96f361fdc137533debb887bded6a5d62bf10488e
2020bz <zcpeterson05@gmail.com>
2026-04-28 19:24:22 -0600

    Working on encryption and decrypt

--- Text.py
+++ Text.py
@@ -1,11 +1,11 @@
 import rsaKey as rsa
 import KeyGenerator as keygen
 
-pub, pri = keygen.generate_keypair(1024, 5915587277,  1500450271 )
+pub, pri = keygen.generate_keypair(1024 )
 print("pub:", pub.e, pub.n)
 print("pri:", pri.e, pri.n)
 
-text = pub.encrypt("HELLO")
+text = pub.encrypt("HELLO I AM ZACHARY PETERSON AND I AM MAKEING THIS A VERY LONG TEXT STRING")
 print(text)
 text = pri.decrypt(text)
 print(text)
--- rsaKey.py
+++ rsaKey.py
@@ -1,5 +1,6 @@
 import csv
 import base64
+import math
 
 class rsaKey:
     """
@@ -9,13 +10,14 @@ class rsaKey:
     along with metadata about the key generation process. Its simple, and easy.
     """
 
-    def __init__(self, e, n):
+    def __init__(self, e, n, bitlength):
         """
         Initialize an RSA key with all necessary components.
 
         Args:
             e (int): exponent
             n (int): RSA modulus (product of two primes)
+            bitlength: Number of encodable bytes
         """
 
         # enforcing that variables are what they are supposed to be.
@@ -28,6 +30,11 @@ class rsaKey:
             self.n = 5
         else:
             self.n = n
+                
+        if(not isinstance(bitlength, int) or bitlength < 0):
+            self.bitlength = 8
+        else:
+            self.bitlength = bitlength
 
     def to_dict(self):
         """
@@ -39,6 +46,7 @@ class rsaKey:
         return {
             'n': self.n,
             'e': self.e,
+            'bitlength': self.bitlength
         }
 
     def save_to_file(self, filename):
@@ -51,10 +59,10 @@ class rsaKey:
         """
         with open(filename, 'w', newline='') as f:
             writer = csv.writer(f, delimiter='\t')
-            writer.writerow([self.e, self.n])
+            writer.writerow([self.e, self.n, self.bitlength])
 
     @staticmethod
-    def load_from_file(cls, filename):
+    def load_from_file(filename):
         """
         Load an RSA key from a TSV file.
 
@@ -67,16 +75,46 @@ class rsaKey:
         with open(filename, 'r') as f:
             reader = csv.reader(f, delimiter='\t')
             data = next(reader)
-            return rsaKey(data[0], data[1])
+            return rsaKey(data[0], data[1], data[2])
 
-    def encrypt(self, text):
-        buffer = int.from_bytes(bytes(text,"utf-8"))
-        buffer = pow(buffer,self.e,self.n)
-        buffer = buffer.to_bytes((buffer.bit_length() + 7) // 8)
-        return base64.b64encode(buffer)
+    def encrypt(self, text: str):
+        """
+        Encrypt text using this key
 
-    def decrypt(self, text):
-        buffer = int.from_bytes(base64.b64decode(text))
-        buffer = pow(buffer, self.e, self.n)
-        buffer = buffer.to_bytes((buffer.bit_length() + 7) // 8)
-        return buffer.decode("utf-8")
+        Args:
+            text (str): Text to encrypt
+        """
+        # Determin the block size
+        # If n bytes are required to store n, we need n - 1 bytes
+        # so that a < n
+        text_len = len(text)
+        block_size = math.floor(math.log2(self.n)) - 1
+        num_blocks = math.ceil(text_len/block_size)
+        all_bytes = bytearray(block_size * num_blocks)
+        for c in range(0, text_len):
+            all_bytes[c] = text[c]
+        blocks = all_bytes[0::block_size]
+        for x in range(0, num_blocks):
+            number = pow(int.from_bytes(blocks[x]), self.e, self.n)
+            block = number.to_bytes(block_size)
+            for c in range(0, block_size):
+                all_bytes[(x*block_size)+c] = block[c]
+        return all_bytes;
+
+    def decrypt(self, all_bytes: bytes):
+        """
+        Decrypt text using this key
+
+        Args:
+            text (bytes): Base64 text to decrypt
+        """
+        num_bytes = len(all_bytes)
+        block_size = math.floor(math.log2(self.n)) - 1
+        num_blocks = math.ceil(num_bytes/block_size)
+        blocks = all_bytes[0::block_size]
+        for x in range(0, num_blocks):
+            number = pow(int.from_bytes(blocks), self.e, self.n)
+            block = number.to_bytes(block_size)
+            for c in range(0, block_size):
+                all_bytes[(x*block_size)+c] = block[c]
+        return all_bytes.encode()
